c065475a467cbd7e36d5efd9121d8ffb6325d84f
Anthony Barragan <anthony.bar.89@gmail.com>
2022-02-15 18:51:49 -0800

    finished site

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
 "use strict";
 const inquirer = require('inquirer');
-
 const fs = require('fs');
 const generatePage = require('./src/page-template.js');
-const { generate } = require('rxjs');
-const { json } = require('stream/consumers');
-const { lookupService } = require('dns');
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile("/index.html", pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log("Portfolio complete! Chceck out index.htnl to see the output!")
-// });
+const { writeFile, copyFile} = require('./utils/generate-site.js');
 
 const promptUser = () => {
     // returns a promise
@@ -137,64 +127,26 @@ const promptProject = portfolioData => {
             }
         })
 }
-const mockData = {
-    name: 'Lernantino',
-    github: 'lernantino',
-    confirmAbout: true,
-    about:
-        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.',
-    projects: [
-        {
-            name: 'Run Buddy',
-            description:
-                'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-            languages: ['HTML', 'CSS'],
-            link: 'https://github.com/lernantino/run-buddy',
-            feature: true,
-            confirmAddProject: true
-        },
-        {
-            name: 'Taskinator',
-            description:
-                'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-            languages: ['JavaScript', 'HTML', 'CSS'],
-            link: 'https://github.com/lernantino/taskinator',
-            feature: true,
-            confirmAddProject: true
-        },
-        {
-            name: 'Taskmaster Pro',
-            description:
-                'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-            languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
-            link: 'https://github.com/lernantino/taskmaster-pro',
-            feature: false,
-            confirmAddProject: true
-        },
-        {
-            name: 'Robot Gladiators',
-            description:
-                'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
-            languages: ['JavaScript'],
-            link: 'https://github.com/lernantino/robot-gladiators',
-            feature: false,
-            confirmAddProject: false
-        }
-    ]
-};
-const pageHTML = generatePage(mockData)
-fs.writeFile('./index.html', pageHTML, err => {
-    if (err) throw new Error(err)
-})
 
-
-// promptUser()
-//     .then(promptProject)
-//     .then(portfolioData => {
-//         const pageHTML = generatePage(portfolioData);
-
-    //     fs.writeFile('./index.html', pageHTML, err => {
-    //     if (err) throw new Error(err);
-    //     console.log('Page created! check out index.html in this directory to see it!')
-    // })
-// });
+// Ask user info
+promptUser()
+    // ask for project info(recusive for multiple objects)
+    .then(promptProject)
+    .then(portfolioData => {
+        return generatePage(portfolioData)
+    })
+    .then(pageHTML => {
+        // need a return so this promise is sent down the 'then' chain.
+        return writeFile(pageHTML);
+    })
+    // the functions in the thens also need to be promises to chain. T of F
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
+    })
--- src/page-template.js
+++ src/page-template.js
@@ -1,5 +1,4 @@
 "use strict";
-
 // create the about section 
 const generateAbout = aboutText => {
     if (!aboutText) {
@@ -29,7 +28,7 @@ const generateProjects = projectsArr => {
                             ${languages.join(", ")}
                             </h5>
                             <p>${description}</p>
-                            <a href="${link} class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+                            <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
                         </div>
                     `;
                 })
